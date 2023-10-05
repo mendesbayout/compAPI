1afb958b9ec129eb78d11c62a38b245c3509f328
Francisco Petry Rauber <xico.petry@gmail.com>
2023-10-05 09:15:20 -0300

    add admin

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,22 @@
+from django import forms
 from django.contrib import admin
+from django.forms import Textarea
+from .models import Despacho
 
-# Register your models here.
+
+class DespachoAdminForm(forms.ModelForm):
+  class Meta:
+    model = Despacho
+    widgets = {
+      'text': Textarea(),
+    }
+    fields = '__all__'
+
+
+
+class DespachoAdmin(admin.ModelAdmin):
+    form = DespachoAdminForm
+    list_display = ('_id', 'url', 'title', 'text', 'date', 'tipo')
+
+
+admin.site.register(Despacho, DespachoAdmin)
