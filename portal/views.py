cc59073d65045c1e9379282c83d7c168c99c2354
Francisco Petry Rauber <xico.petry@gmail.com>
2023-10-05 15:06:48 -0300

    add get normas

--- portal/views.py
+++ portal/views.py
@@ -5,32 +5,28 @@ from portal.models import Despacho
 # Create your views here.
 
 
-def add_despacho(d):
-    despacho = Despacho(
-        _id = str(d['_id']),
-        url = d['url'],
-        title = d['title'],
-        text = d['text'],
-        date = d['date'],
-        tipo = d['tipo']
-    )
-    despacho.save()
-    return despacho
 
-def get_despachos():
-    client = pymongo.MongoClient("mongo", 27017)
-    db = client["mydb"]
-    despachos = db.despachos
-    d = despachos.find_one()
-    return d
+def get_normas():
+    MONGODB_URI = "mongodb+srv://lucas:bayout@cluster0.uqxmkzv.mongodb.net/?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(MONGODB_URI)
+    db = client["complianceRAW"]
+    collection_names = db.list_collection_names()
+    normas = []
+    for collection_name in collection_names:
+        c = 0
+        for norma in db[collection_name].find({}).limit(10):
+            if c > 3:
+                break
+            normas.append(norma)
+    return normas
+
 
 class IndexView(TemplateView):
     template_name = "portal/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        client = pymongo.MongoClient("mongo", 27017)
-        db = client["mydb"]
-        despachos = db.despachos
-        context['despachos'] = despachos.find({})
+
+        context['normas'] = get_normas()
+
         return context
